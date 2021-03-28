cf7bdddafc0ba8007b91b9ff5579f0874415b895
Victor-Tilve <ing.victortilve@gmail.com>
2021-03-28 18:20:55 -0500

    I'm trying to not fuck thi up

---
+++ 00. Search/Search-Algorithm-Maze/src/node.py
@@ -0,0 +1,15 @@
+'''
+Created on 7/03/2021
+
+@author: VATS
+'''
+
+class Node():
+    '''
+    classdocs
+    '''
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
